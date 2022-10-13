4310287937ec5f186d163467643823587fda448e
Ansen123 <ansenfp@gmail.com>
2022-10-14 00:11:01 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './navbar/navbar.component';
+import { AddFriendsComponent } from './add-friends/add-friends.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ViewFriendsComponent } from './view-friends/view-friends.component';
+const appRoutes:Routes=[
+  {
+    path:"",component:AddFriendsComponent
+  },
+  {
+    path:"view",component:ViewFriendsComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AddFriendsComponent,
+    ViewFriendsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
